4a2b279666e84e9b763558340cee54b6ab876edb
Song Zhang <songhui@ualberta.ca>
2021-05-05 14:50:52 -0600

    op_gen

--- op_gen.py
+++ op_gen.py
@@ -1,3 +1,9 @@
 with open('input', 'r') as a:
   with open('output', 'w') as b:
-    print(a.read())
+    num = 0
+    for line in a:
+      line = line.strip()
+      b.write('def {}():\n'.format(line))
+      b.write('\t#{}\n'.format(hex(num)))
+      b.write('\tpass\n\n')
+      num += 1
